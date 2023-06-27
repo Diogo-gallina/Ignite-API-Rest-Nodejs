02e8eab3e059d0ad7c53aa6d837e782b87c62b6a
Diogo-gallina <diogogallina20@gmail.com>
2023-06-27 13:27:48 -0300

    feat: create- transaction-POST

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,12 @@ import fastify from "fastify";
 import { knex } from "./database";
 import crypto from "node:crypto";
 import { env } from "./env";
+import { transactionsRoutes } from "./routes/transactions";
 
 const app = fastify();
 
-app.get('/hello', async () => {
-    const transaction = await knex('transactions')
-    .where('amount', 1000)
-    .select('*');
-
-    return transaction;
+app.register(transactionsRoutes, {
+    prefix: 'transactions'
 });
 
 app.listen({
