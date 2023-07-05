b81cbffc1646db4f27350e2c9f7323a4c37408ad
Diogo-gallina <diogogallina20@gmail.com>
2023-07-05 08:49:49 -0300

    feat: add-test-list-all-transactions

--- test/example.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from 'vitest';
-import { app } from 
-
-test('o usuário consegue criar uma nova transação', () => {
-
-    const responseStatusCode = 201
-
-    expect(responseStatusCode).toEqual(201)
-});
